2e04bf03bc414a739d056792728bb78e4f84d197
StarkDoom <reki_s4@hotmail.com>
2021-07-22 01:18:05 -0300

    commit 03

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -3,7 +3,7 @@
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
-  // Si son iguales, devuelve cualquiera de los dos
+  // Si son iguales, devuelve cualquiera de los dos.
   // Tu código:
   if (x > y) {
     return x;
