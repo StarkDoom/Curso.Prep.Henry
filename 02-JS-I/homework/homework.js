3809f9b6aee3fcf8516c06a22eaec91f2deb6773
StarkDoom <reki_s4@hotmail.com>
2021-07-22 01:19:35 -0300

    commit 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -33,7 +33,7 @@ function devolverString(str) {
 
 function suma(x, y) {
   // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
+  // Suma "x" e "y" juntos y devuelve el valor.
   // Tu código:
   return x + y;
 }
